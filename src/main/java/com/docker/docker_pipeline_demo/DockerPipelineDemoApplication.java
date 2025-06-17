eddad188c5f08604a130dabbeedfe5ce1f917b3e
Karun <karun.np@gmail.com>
2025-06-17 17:45:40 +0530

    first commit

---
+++ src/main/java/com/docker/docker_pipeline_demo/DockerPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.docker.docker_pipeline_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/docker_pipeline_demo/DockerPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.docker_pipeline_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
