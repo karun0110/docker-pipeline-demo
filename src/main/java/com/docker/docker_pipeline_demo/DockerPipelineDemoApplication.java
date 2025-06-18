bdd6a2debc8207947100abece38cff0bf4257314
Karun <karun.np@gmail.com>
2025-06-18 12:15:59 +0530

    added get request and updated java version

--- src/main/java/com/docker/docker_pipeline_demo/DockerPipelineDemoApplication.java
+++ src/main/java/com/docker/docker_pipeline_demo/DockerPipelineDemoApplication.java
@@ -2,10 +2,17 @@ package com.docker.docker_pipeline_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerPipelineDemoApplication {
 
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Spring docker pipeline project";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DockerPipelineDemoApplication.class, args);
 	}
